7ed9572e0b983fd59082765ac2b7d2bdc3e33710
harryapp <70029200+harryapp@users.noreply.github.com>
2022-11-30 11:35:21 +0100

    add sum functions

--- src/main/java/no/shoppifly/Cart.java
+++ src/main/java/no/shoppifly/Cart.java
@@ -11,6 +11,14 @@ import java.util.List;
 public class Cart {
     private String id;
     List<Item> items = new ArrayList<>();
+    private boolean isCheckedout;
+
+    public float getCartSum(){
+        float sum = 0;
+        for(Item i : items)
+            sum += i.getSum();
+        return sum;
+    }
 }
 
 @Data
@@ -20,4 +28,7 @@ class Item {
     private int qty;
     private Float unitPrice;
 
+    public float getSum(){
+        return unitPrice * qty;
+    }
 }
