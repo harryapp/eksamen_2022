1601c16d1559ec33bf16a18a1e13b6bdcaf0a4a6
harryapp <70029200+harryapp@users.noreply.github.com>
2022-12-01 15:00:32 +0100

    fix workflow

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -3,7 +3,3 @@
 resource "aws_s3_bucket" "analyticsbucket" {
   bucket = "analytics-${var.candidate_id}"
 }
-
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-${var.candidate_id}"
-}
