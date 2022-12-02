f955247edfbb5da08bbf42b498c1d225f6762302
harryapp <70029200+harryapp@users.noreply.github.com>
2022-12-03 00:39:10 +0100

    test dashboard

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -71,7 +71,7 @@ resource "aws_cloudwatch_dashboard" "main" {
       "height": 6,
       "properties": {
         "metrics": [
-          [ { "expression": "SELECT MAX(CPUUtilization) FROM SCHEMA(\"AWS/EC2\", InstanceId) GROUP BY InstanceId LIMIT 10", "label": "View the 10 max CPU Utilization", "id": "q1" } ]
+          [ { "expression": "SELECT AVG("checkout_latency.avg") FROM SCHEMA(\"1037\", class,\"exception\",\"method\") WHERE "exception" = 'none' AND "method" = 'checkout' AND class = 'no.shoppifly.ShoppingCartController' } ]
         ],
         "period": 300,
         "stat": "Average",
