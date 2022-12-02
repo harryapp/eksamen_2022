6b1d3a09634a12311517dff7315d18c4ff860d4c
harryapp <70029200+harryapp@users.noreply.github.com>
2022-12-03 00:46:55 +0100

    test dashboard

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -71,7 +71,7 @@ resource "aws_cloudwatch_dashboard" "main" {
       "height": 6,
       "properties": {
         "metrics": [
-          [ { "expression": "SELECT AVG("checkout_latency.avg") FROM SCHEMA(\"1037\", class,"exception","method") WHERE "exception" = 'none' AND "method" = 'checkout' AND class = 'no.shoppifly.ShoppingCartController'" } ]
+          [ { "expression": "SELECT AVG(\"checkout_latency.avg\") FROM SCHEMA(\"1037\", class,\"exception\",\"method\") WHERE \"exception\" = 'none' AND \"method\" = 'checkout' AND class = 'no.shoppifly.ShoppingCartController'" } ]
         ],
         "period": 300,
         "stat": "Average",
