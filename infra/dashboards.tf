0f53327321d6b617da854cc93660674dfba8635b
harryapp <70029200+harryapp@users.noreply.github.com>
2022-12-02 23:51:44 +0100

    test dashboard

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -74,7 +74,12 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "checkout_latency.avg"
+            "checkout_latency.avg",
+            [
+              {"class", "no.shoppifly.ShoppingCartController"},
+              {"expection", "none"},
+              {"method", "checkout"}
+            ]
           ]
         ],
         "period": 300,
